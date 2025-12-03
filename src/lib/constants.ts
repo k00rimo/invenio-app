78be45ec1c1f3597c90c8653a167846a562aa3f5
Filip Mudry <fmudry@redhat.com>
2025-12-03 16:20:30 +0100

    delete unused files

--- src/lib/constants.ts
+++
@@ -1,7 +0,0 @@
-export const QUERY_PARAM_SORT = 'sort';
-export const QUERY_PARAM_SIZE = 'size';
-export const QUERY_PARAM_PAGE = 'page';
-
-export const DEFAULT_SORT = 'bestmatch';
-export const DEFAULT_SIZE = '10';
-export const DEFAULT_COMMUNITY_SIZE = '5';
