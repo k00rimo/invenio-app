64368356f1f95c328bd965c0b05fd55bd4076993
Filip Mudry <fmudry@redhat.com>
2025-12-03 16:25:10 +0100

    export api url

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,6 +1,6 @@
 import axios, { type AxiosInstance } from "axios";
 
-const API_URL = "https://mdposit.mddbr.eu/api/rest/v1";
+export const API_URL = "https://mdposit.mddbr.eu/api/rest/v1";
 
 const apiClient: AxiosInstance = axios.create({
   baseURL: API_URL,
